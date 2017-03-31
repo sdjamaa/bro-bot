7a0c82111736045bc081de7c517ecf220223e364
Yacine Achiakh <y.achiakh@criteo.com>
2017-03-30 20:11:34 -0700

    modify filter

--- src/main/scala/com/criteo/bro/KeywordFilter.scala
+++ src/main/scala/com/criteo/bro/KeywordFilter.scala
@@ -1,7 +1,7 @@
 package com.criteo.bro
 
 object KeywordFilter {
-  val phrases = List("I want", "Do you have", "Is there", "Can I buy", "Can I get")
+  val phrases = List("I want", "Do you have", "Is there", "Can I buy", "Can I get", "Can I", "Hello", "Hi")
   val phraseRegex = phrases.tail.foldLeft(phrases.head)((a, b) => s"$a|$b")
   val phraseKeywordRegex = s"""($phraseRegex)\\s+(a\\s+)?(([a-z]+ )*[a-z]+)""".r
 
