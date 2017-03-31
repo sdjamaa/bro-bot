70bc2b00a17d256f97e63342039c483328f96b86
Ahmed Shamim <a.shamim@criteo.com>
2017-03-31 08:53:07 -0700

    Support audio urls (Depends on ffmpeg)

--- src/main/scala/com/criteo/bro/SpeechToText.scala
+++ src/main/scala/com/criteo/bro/SpeechToText.scala
@@ -1,8 +1,11 @@
 package com.criteo.bro
 
 import java.io.File
-import collection.JavaConversions._
+import java.net.URL
+import sys.process._
 
+import collection.JavaConversions._
+import org.apache.commons.io.FileUtils
 import com.ibm.watson.developer_cloud.speech_to_text.v1._
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.{SpeechAlternative, SpeechResults, Transcript}
 
@@ -10,19 +13,45 @@ object SpeechToText {
   val service: SpeechToText = new SpeechToText
   service.setUsernameAndPassword("1d782f20-5182-48a0-aa4b-c58fc1130549", "yylJfj2pYAZ7")
 
-  def convertSpeechToText(file: String): String = {
-    val audio: File = new File(file)
+  def convertAudioUrlToText(urlString: String): String = {
+    val url = new URL(urlString)
+    val extension = urlString substring urlString.lastIndexOf(".")
+    val tDir = System.getProperty("java.io.tmpdir")
+    val path = tDir + "tmp" + extension
+    val wavPath = tDir + "tmp" + ".wav"
+    val file = new File(path)
+    file.deleteOnExit
 
-    val transcript: SpeechResults = service.recognize(audio).execute
-    var outputString = ""
-    val confidence: Double = 0
-    for (item: Transcript <- transcript.getResults) {
-      for (alternative: SpeechAlternative <- item.getAlternatives) {
-        if (alternative.getConfidence > confidence)
-          outputString = alternative.getTranscript
-      }
+    try {
+      FileUtils.copyURLToFile(url, file)
+      s"ffmpeg -i $path $wavPath" !
+      val wavFile = new File(wavPath)
+      wavFile.deleteOnExit
+      convertSpeechToText(wavFile)
+    } catch {
+      case e: Exception => ""
     }
-    outputString stripSuffix " "
   }
 
+  def convertSpeechToText(file: String): String = {
+    val audio: File = new File(file)
+    convertSpeechToText(audio)
+  }
+
+  def convertSpeechToText(audio: File): String = {
+    try {
+      val transcript: SpeechResults = service.recognize(audio).execute
+      var outputString = ""
+      val confidence: Double = 0
+      for (item: Transcript <- transcript.getResults) {
+        for (alternative: SpeechAlternative <- item.getAlternatives) {
+          if (alternative.getConfidence > confidence)
+            outputString = alternative.getTranscript
+        }
+      }
+      outputString stripSuffix " "
+    } catch {
+      case e: Exception => ""
+    }
+  }
 }
--- src/test/scala/com/criteo/bro/VoiceRecognizer.scala
+++ src/test/scala/com/criteo/bro/VoiceRecognizer.scala
@@ -4,7 +4,14 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class VoiceRecognizer extends FlatSpec with Matchers {
   it should "match" in {
-    val result = SpeechToText.convertSpeechToText("/users/a.shamim/Downloads/test.wav")
+    val result = SpeechToText.convertSpeechToText("test.wav")
     result shouldEqual "can I get a beer"
+  }
+
+  it should "match2" in {
+    val result = SpeechToText.convertAudioUrlToText("https://aacapps.com/lamp/sound/amy.mp3")
+    val expectedResult = "hi I'm Amy one of the available high quality text to speech voices select download now to install my voice"
+
+    result shouldEqual expectedResult
   }
 }
