f96d94c3a5ec1780260c3dc39d1e02309c902619
Ahmed Shamim <a.shamim@criteo.com>
2017-03-30 21:11:34 -0700

    Add stopwords to keyword filter

--- src/test/scala/com/criteo/bro/FilterKeywords.scala
+++ src/test/scala/com/criteo/bro/FilterKeywords.scala
@@ -7,14 +7,21 @@ class FilterKeywords extends FlatSpec with Matchers {
   it should "match1" in {
     val input = "I want a grey, sweatshirt"
     val output = KeywordFilter.extractKeywords(input)
-    val expectedOutput = List("grey", "sweatshirt")
+    val expectedOutput = "grey sweatshirt"
     output shouldEqual expectedOutput
   }
 
   it should "match2" in {
     val input = "Do you have a blue printer?"
     val output = KeywordFilter.extractKeywords(input)
-    val expectedOutput = List("blue", "printer")
+    val expectedOutput = "blue printer"
+    output shouldEqual expectedOutput
+  }
+
+  it should "match3" in {
+    val input = "Can I get an HDMI?"
+    val output = KeywordFilter.extractKeywords(input)
+    val expectedOutput = "hdmi"
     output shouldEqual expectedOutput
   }
 
